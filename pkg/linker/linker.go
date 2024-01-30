cd8b83d84e4bbca2a5872ca61b571383e9ceb43e
Julian Klaiber <julian@klaiber.me>
2024-01-30 15:32:50 +0100

    Update

---
+++ pkg/linker/linker.go
@@ -0,0 +1,55 @@
+package linker
+
+import (
+	"fmt"
+
+	"github.com/jklaiber/ebpf-bridge/pkg/logging"
+)
+
+var log = logging.DefaultLogger.WithField("subsystem", Subsystem)
+
+const Subsystem = "linker"
+
+type Linker interface {
+	Attach() error
+	Detach() error
+}
+
+type XdpLinker struct {
+	iface   netlink.Link
+	program *ebpf.Program
+	link    link.Link
+}
+
+func NewXdpLinker(iface netlink.Link, program *ebpf.Program) *XdpLinker {
+	return &XdpLinker{
+		iface:   iface,
+		program: program,
+	}
+}
+
+func (x *XdpLinker) Attach() error {
+	log.Infof("Attaching XDP program to %s", x.iface.Attrs().Name)
+	if x.program == nil {
+		return fmt.Errorf("cannot attach a nil program")
+	}
+	link, err := link.AttachXdp(link.XDPOptions{
+		Program:   x.program,
+		Interface: l.iface.Attrs().Index,
+		Flags:     link.XDPGenericMode,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to attach XDP program to interface %s: %w", l.iface.Attrs().Name, err)
+	}
+	l.link = link
+	return nil
+}
+
+func (x *XdpLinker) Detach() error {
+	log.Infof("Detaching XDP program from %s", x.iface.Attrs().Name)
+	err := x.link.Close()
+	if err != nil {
+		return fmt.Errorf("failed to detach XDP program: %w", err)
+	}
+	return nil
+}
